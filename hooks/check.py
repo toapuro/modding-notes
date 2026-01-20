1893cd6d386b83a9e5502d282af968004856d53a
toapuro <164835903+toapuro@users.noreply.github.com>
2026-01-20 15:53:35 +0000

    Ci: テストCIを追加、CI連携を強化

--- hooks/check.py
+++ hooks/check.py
@@ -13,6 +13,13 @@ def on_page_markdown(markdown, page, **kwargs):
     path = page.file.src_uri
     
     for annotation in ANNOTATIONS:
-        for m in re.finditer(f'({annotation}): ?(.*)', markdown):
-            log.warning(f"Doc file '{path}' contains {annotation}: {m[0]}")
-            
+        for m in re.finditer(f'<!-- ({annotation}): ?(.*) -->', markdown):
+            text = f"Doc file '{path}' contains {annotation}: {m[2]}"
+
+            if(annotation == "NOTE"):
+                log.info(text)
+                print(f"::notice title=mkdocs::{text}")
+                continue
+
+            log.warning(text)
+            print(f"::warning title=mkdocs::{text}")
